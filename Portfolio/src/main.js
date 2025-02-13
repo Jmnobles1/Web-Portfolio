f6fec10b2a66ebf819cb0010d63a62f83ce72d8a
Jaden Nobles <jmnobles1@aggies.ncat.edu>
2025-02-13 15:52:54 -0500

    push new changes

--- Portfolio/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import VueSelect from "vue-select";
-
-const app = createApp(App);
-app.component('v-select',VueSelect)
-app.mount('#app')
-
